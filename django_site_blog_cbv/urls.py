2e676dbac4190a3608501108d3033929388f3777
Sergey <main0serg@gmail.com>
2024-09-20 12:46:45 +0600

    added profile template and views

--- django_site_blog_cbv/urls.py
+++ django_site_blog_cbv/urls.py
@@ -23,6 +23,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.blog.urls")),
+    path("", include("apps.accounts.urls")),
 ]
 
 if settings.DEBUG:
